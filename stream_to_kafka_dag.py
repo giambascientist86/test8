843d3d3f2ecec8fb9fcae6324b87826821d94746
Giovanni Battista R <155755287+giambascientist86@users.noreply.github.com>
2024-01-15 13:30:58 +0100

    Add files via upload

---
+++ stream_to_kafka_dag.py
@@ -0,0 +1,37 @@
+
+# Importing modules
+import airflow
+import kafka_streaming_file
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from kafka_streaming_file import initiate_stream
+# Configuration for the DAG's start date
+DAG_START_DATE = datetime(2023, 1, 15, 13, 40)
+
+# Default arguments for the DAG
+DAG_DEFAULT_ARGS = {
+    'owner': 'airflow',
+    'start_date': DAG_START_DATE,
+    'retries': 1,
+    'retry_delay': timedelta(seconds = 5)
+}
+
+# Creating the DAG with its configuration
+with DAG(
+    'movie_rec_dag',  
+    default_args=DAG_DEFAULT_ARGS,
+    schedule_interval='0 1 * * *',
+    catchup=False,
+    description='Stream movies to Kafka topic movies',
+    max_active_runs=1
+) as dag:
+    
+    # Defining the data streaming task using PythonOperator
+    kafka_stream_task = PythonOperator(
+        task_id = 'stream_to_kafka_task', 
+        python_callable = initiate_stream,
+        dag = dag
+    )
+
+    kafka_stream_task
